729fb2ddd4b65b7b7c2053dfb45630e722d52541
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-10-02 12:30:05 +0200

    Add errors on INSTALL or LOAD errors

--- src/extension_alias.c
+++ src/extension_alias.c
@@ -11,9 +11,11 @@
 
 DUCKDB_EXTENSION_ENTRYPOINT(duckdb_connection connection, duckdb_extension_info info, struct duckdb_extension_access *access) {
 	if (duckdb_query(connection, "INSTALL " STR(DUCKDB_EXTENSION_CANONICAL) " FROM community;", NULL) == DuckDBError){
+		access->set_error(info, "Nested install of `" STR(DUCKDB_EXTENSION_CANONICAL) "` failed");
 		return false;
 	}
 	if (duckdb_query(connection, "LOAD " STR(DUCKDB_EXTENSION_CANONICAL), NULL) == DuckDBError){
+		access->set_error(info, "Nested load of `" STR(DUCKDB_EXTENSION_CANONICAL) "` failed");
 		return false;
 	}
 	return true;
